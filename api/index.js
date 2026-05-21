1cc81cddc27f3e815abf9b9dce89618e85215cce
Kr4t0suu <reidazueira098@gmail.com>
2026-05-21 02:02:02 -0400

    fix mercado pago vercel

--- api/index.js
+++ api/index.js
@@ -1,6 +1,10 @@
 import express from "express";
 import cors from "cors";
-import mercadopago from "mercadopago";
+
+import {
+  MercadoPagoConfig,
+  Preference,
+} from "mercadopago";
 
 const app = express();
 
@@ -8,32 +12,40 @@ app.use(cors());
 
 app.use(express.json());
 
-mercadopago.configure({
-  access_token: process.env.MP_ACCESS_TOKEN,
+const client = new MercadoPagoConfig({
+  accessToken: process.env.MP_ACCESS_TOKEN,
 });
 
 app.post("/api", async (req, res) => {
   try {
     const { itens } = req.body;
 
-    const preference = {
-      items: itens.map((item) => ({
-        title: item.nome,
-        unit_price: Number(item.preco),
-        quantity: Number(item.quantidade),
-        currency_id: "BRL",
-      })),
-    };
+    if (!itens || itens.length === 0) {
+      return res.status(400).json({
+        erro: "Carrinho vazio",
+      });
+    }
 
-    const resposta = await mercadopago.preferences.create(
-      preference
-    );
+    const items = itens.map((item) => ({
+      title: item.nome,
+      quantity: Number(item.quantidade || 1),
+      currency_id: "BRL",
+      unit_price: Number(item.preco || 0),
+    }));
+
+    const preference = new Preference(client);
+
+    const response = await preference.create({
+      body: {
+        items,
+      },
+    });
 
     res.json({
-      init_point: resposta.body.init_point,
+      init_point: response.init_point,
     });
   } catch (erro) {
-    console.log(erro);
+    console.log("ERRO:", erro);
 
     res.status(500).json({
       erro: "Erro ao gerar pagamento",
